12b7d32b42854d8c024f2d7934350c2bc8d25d02
Wisnu Danuarta <60722042+Waizsec@users.noreply.github.com>
2023-11-12 15:34:06 +0700

    Update insertdata.php

--- uts/insertdata.php
+++ uts/insertdata.php
@@ -1,15 +1,11 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    function sanitizeInput($input) {
-        return htmlspecialchars(trim($input));
-    }
-
-    $id = sanitizeInput($_POST['id']);
-    $f_name = sanitizeInput($_POST['f_name']);
-    $l_name = sanitizeInput($_POST['l_name']);
-    $email = sanitizeInput($_POST['email']);
-    $email2 = sanitizeInput($_POST['email2']);
-    $profesi = sanitizeInput($_POST['profesi']);
+    $id = $_POST['id'];
+    $f_name = $_POST['f_name'];
+    $l_name = $_POST['l_name'];
+    $email = $_POST['email'];
+    $email2 = $_POST['email2'];
+    $profesi = $_POST['profesi'];
 
     if (empty($id) || empty($f_name) || empty($l_name) || empty($email) || empty($email2) || empty($profesi)) {
         echo "ada yang kosong";
